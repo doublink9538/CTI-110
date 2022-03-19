0cb7aae4ab0429c9986669dd56a1bb1d51eb005a
doublink9538 <99505023+doublink9538@users.noreply.github.com>
2022-03-19 19:22:03 -0400

    Add files via upload
    Turtle Initials Graphic

---
+++ P4LAB2b_Doublin.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.pensize(40)
+turtle.color("purple")
+
+turtle.right(90)
+turtle.forward(200)
+turtle.backward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.backward(100)
+turtle.left(80)
+turtle.forward(100)
+
+turtle.penup()
+turtle.right(75)
+turtle.forward(180)
+turtle.pendown()
+
+turtle.color("teal")
+turtle.left(40)
+turtle.circle(100,180)
+turtle.left(90)
+turtle.forward(190)
+
+turtle.done()
