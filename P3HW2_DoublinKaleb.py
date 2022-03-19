6d6540c97c36cda8b53a6cf08d7fcfeac3270c74
doublink9538 <99505023+doublink9538@users.noreply.github.com>
2022-03-19 19:17:47 -0400

    Add files via upload
    Pizza Order

---
+++ P3HW2_DoublinKaleb.py
@@ -0,0 +1,23 @@
+#CTI-110
+#P3HW2-Pizza Order
+#Kaleb Doublin
+#03/11/2022
+def main():
+    import math
+
+    students = int(input('Enter the number of students: '))
+    people = float(input('Enter the number of people per pizza: '))
+
+    if(people == 1.5 or people == 2 or people == 3):
+	    whole_pizzas =  math.ceil(students / people)
+	    price = whole_pizzas * 5
+	    price = price + (price * 0.06)
+	    print('Number of whole pizzas: ', whole_pizzas)
+	    print('Price: $', price)
+    else:
+	    print('Error: Please enter 1.5, 2, or 3')
+	    print('Please run program again')
+
+main()
+
+
