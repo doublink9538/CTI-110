e2755caf484704b8217373e66929de24c8e3fc07
doublink9538 <99505023+doublink9538@users.noreply.github.com>
2022-03-19 19:13:09 -0400

    Add files via upload
    Score Average

---
+++ P2HW2_Doublin_Kaleb.py
@@ -0,0 +1,34 @@
+# Test Scores HW
+# 02/22/22
+# CTI-110 P2HW2 – Score_Avg
+#Kaleb Doublin
+#
+num_list = []
+num1 = float(input("Enter your first test score: "))
+num_list.append(num)
+num2 = float(input("Enter your second test score: "))
+num_list.append(num)
+num3 = float(input("Enter your third test score: "))
+num_list.append(num)
+num4 = float(input("Enter your fourth test score: "))
+num_list.append(num)
+num5 = float(input("Enter your fifth test score: "))
+num_list.append(num)
+num6 = float(input("Enter your sixth test score: "))
+num_list.append(num)
+"num7 = float(input(“Enter your seventh test score: “))
+num_list.append(num)
+
+print(num_list)
+lowest = min(num_list)
+
+num_list.remove(lowest)
+print(num_list)
+total = sum(num_list)
+length = len(num_list)
+average = total/length
+
+
+print('Lowest ‘, lowest)
+print("Modified List: ,num_list)
+print("Average: “, average)
